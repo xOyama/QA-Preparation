2da33759d2f18d21c561be7981da9394abab3461
xOyama <paberukun@gmail.com>
2020-11-23 16:58:21 +0200

    Telerik

---
+++ src/com/telerikacademy/SumOf3Numbers.java
@@ -0,0 +1,12 @@
+package com.telerikacademy;
+import java.util.Scanner;
+
+public class SumOf3Numbers {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+        
+
+
+    }
+}
